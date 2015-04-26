e40e851b99e031d2724a12ee17b43d640d092ac1
supistar <supistars@gmail.com>
2015-04-26 12:07:10 +0900

    Fix setMinMax and removeFragments logic with async function.

--- ice.js
+++ ice.js
@@ -51,39 +51,19 @@
   }
 
   var timeout;
-  var resourceWaitTime = 30 * 1000;
+  var resourceWaitTime = 10 * 1000;
   var receiveTimer;
   page.onResourceReceived = function(response) {
     debug("Received : " + receiveTimer);
     if (receiveTimer) {
       clearTimeout(receiveTimer);
     }
-    receiveTimer = setTimeout(loadingMessageTimer, resourceWaitTime);
+    receiveTimer = setTimeout(loadingMessageCheckQueue, resourceWaitTime);
   };
 
   // Loading message detection
-  var loadWaitTime = 30 * 1000;
-  var loadTimer;
-
-  function loadingMessageTimer() {
-    debug("LoadTimer: " + loadTimer);
-    if (loadTimer) {
-      clearTimeout(loadTimer);
-    }
-    loadTimer = setTimeout(function() {
-      debug("Timeout fired");
-      var loadmessage = page.evaluate(function() {
-        return document.querySelector('#loading_msg');
-      });
-      if (loadmessage && loadmessage.style && loadmessage.style.display == "none") {
-        debug("Capture service fired");
-        captureService();
-      } else {
-        debug("Capture service not fired :(");
-        loadingMessageTimer();
-      }
-    }, loadWaitTime);
-  };
+  var loadWaitTime = 5 * 1000;
+  var loadTimer = 0;
 
   page.settings.userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36";
   page.settings.resourceTimeout = timeoutTime;
@@ -282,16 +262,17 @@
     }, loginTimeout);
   }
 
-  function removeFragments() {
+  function removeFragments(callback) {
     // Check COMM element is exist or not
     var comm = page.evaluate(function() {
       return document.querySelector('#comm');
     });
     if (comm == null) {
       debug("There is no COMM element");
-      return false;
+      callback("Error!", false);
+      return;
     } else if (comm.style.display != "none") {
-      debug("COMM : " + comm);
+      debug("COMM element is found");
       page.evaluate(function() {
         document.querySelector('#comm').style.display = 'none';
         document.querySelector('#player_stats').style.display = 'none';
@@ -314,58 +295,95 @@
     });
     if (fragment == null) {
       debug("There is no #filters_container element");
-      return false;
+      callback("Error!", false);
+      return;
     } else if (fragment.style.display != "none") {
       if ((minlevel > 1) | (maxlevel < 8)) {
         debug('Set portal level : ' + minlevel + "/" + maxlevel);
-        setMinMax(minlevel, maxlevel);
+        setMinMax(minlevel, maxlevel, callback);
+        return;
       } else {
         page.evaluate(function() {
           document.querySelector('#filters_container').style.display = 'none'
         });
+        callback(null, true);
       }
-      return false;
+    } else {
+      callback(null, true);
     }
-    return true;
   }
 
-  function captureService() {
+  function loadingMessageCheckQueue() {
+    loadingQueue.push(null, function(err) {
+      if (err == null) {
+        screenshot();
+      }
+    });
+    debug("Loading queue added : " + loadingQueue.length());
+  }
+
+  var loadingQueue = async.queue(function(task, queueCallback) {
+    loadTimer++;
+    debug("LoadTimer: " + loadTimer);
     debug("*** CaptureService ***");
-    var pageBase = 'https://www.ingress.com/intel';
-    if (page.url.substring(0, pageBase.length) == pageBase && page.url == pageBase) {
-      debug('It seems to finish authentication. Try to open main logic...');
-      openMain();
-      return;
-    };
-    if (page.url != area) {
-      debug("Page is different: " + page.url);
-      loadingMessageTimer();
-      return;
-    }
-
-    var removed = removeFragments();
-    if (!removed) {
-      debug("fragment is not removed");
-      loadingMessageTimer();
-      return;
-    }
-    setTimeout(function() {
-      debug("Try to detect clip area");
-      var mySelector = "#map_canvas";
-      var elementBounds = page.evaluate(function(selector) {
-        var clipRect = document.querySelector(selector).getBoundingClientRect();
-        return {
-          top: clipRect.top,
-          left: clipRect.left,
-          width: clipRect.width,
-          height: clipRect.height
+    async.series({
+      loadmessage: function(callback) {
+        var loadmessage = page.evaluate(function() {
+          return document.querySelector('#loading_msg');
+        });
+        if (loadmessage && loadmessage.style && loadmessage.style.display == "none") {
+          debug("Capture service fired");
+          callback(null, true);
+        } else {
+          debug("Capture service not fired :(");
+          callback("Error!", false);
+        }
+      },
+      checkPage: function(callback) {
+        var pageBase = 'https://www.ingress.com/intel';
+        if (page.url.substring(0, pageBase.length) == pageBase && page.url == pageBase) {
+          debug('It seems to finish authentication. Try to open main logic...');
+          openMain();
+          callback('Error', false);
+          return;
         };
-      }, mySelector);
-      var oldClipRect = page.clipRect;
-      page.clipRect = elementBounds;
-      screenshot();
-    }, 1000);
-  }
+        if (page.url != area) {
+          debug("Page is different: " + page.url);
+          callback('Error', false);
+          return;
+        }
+        debug("Target page URL is OK");
+        callback(null, true);
+      },
+      checkFragments: function(callback) {
+        removeFragments(callback);
+      },
+      checkClipArea: function(callback) {
+        debug("checkClipArea start");
+        setTimeout(function() {
+          debug("Try to detect clip area");
+          var mySelector = "#map_canvas";
+          var elementBounds = page.evaluate(function(selector) {
+            var clipRect = document.querySelector(selector).getBoundingClientRect();
+            return {
+              top: clipRect.top,
+              left: clipRect.left,
+              width: clipRect.width,
+              height: clipRect.height
+            };
+          }, mySelector);
+          var oldClipRect = page.clipRect;
+          page.clipRect = elementBounds;
+          debug("Clip area detedted");
+          callback(null, true);
+        }, 1000);
+        debug("checkClipArea finish");
+      }
+    }, function(err, results) {
+      debug("*** Check error : " + err + " ***");
+      queueCallback(err);
+    });
+  }, 1);
 
   function openMain() {
     debug("Logic : OpenMain");
@@ -373,87 +391,119 @@
       page.open(area, function() {
         debug("Open : " + area);
         debug('Authenticated successfully, starting screenshotting portals in range between levels ' + minlevel + ' and ' + maxlevel + ' every ' + v / 1000 + 's...');
-        loadingMessageTimer();
       });
     }, 1000);
   }
 
-  function setMinMax(min, max) {
+  function setMinMax(min, max, setCallback) {
     var clickInterval = 3000;
-    setTimeout(function() {
-      var minAvailable = page.evaluate(function() {
-        return document.querySelectorAll('.level_notch.selected')[0];
-      });
-      var maxAvailable = page.evaluate(function() {
-        return document.querySelectorAll('.level_notch.selected')[1];
-      });
-      var currentMinStr = minAvailable.id.replace(/level_(high|low)/g, "");
-      var currentMaxStr = maxAvailable.id.replace(/level_(high|low)/g, "");
-      var currentMin = parseInt(currentMinStr, 10);
-      var currentMax = parseInt(currentMaxStr, 10);
 
-      async.series({
-        min: function(callback) {
-          if (currentMin > min) {
-            debug('The minimal portal level is too low, using default. Consider setting it higher.');
-            callback(null, false);
-            return;
-          }
-          if (currentMin == min) {
-            debug('Min specified: ' + currentMin);
-            callback(null, true);
-            return;
-          }
-          setTimeout(function() {
-            debug('Change Min value: ' + currentMin + '->' + min);
-            var rect = page.evaluate(function() {
-              return document.querySelectorAll('.level_notch.selected')[0].getBoundingClientRect();
-            });
-            debug("Rect click : " + rect.left + rect.width / 2 + " / " + rect.top + rect.height / 2);
-            page.sendEvent('click', rect.left + rect.width / 2, rect.top + rect.height / 2);
-            setTimeout(function() {
-              var rect1 = page.evaluate(function(min) {
-                return document.querySelector('#level_low' + min).getBoundingClientRect();
-              }, min);
-              debug("Rect1 click : " + rect1.left + rect1.width / 2 + " / " + rect1.top + rect1.height / 2);
-              page.sendEvent('click', rect1.left + rect1.width / 2, rect1.top + rect1.height / 2);
-              callback(null, true);
-            }, clickInterval);
-          }, clickInterval)
-        },
-        max: function(callback) {
-          if (currentMax == max) {
-            debug('Max specified: ' + currentMax);
-            callback(null, true);
-            return;
-          }
-          if (max < 8) {
-            debug('Change Max value: ' + currentMax + '->' + max);
-            setTimeout(function() {
-              var rect2 = page.evaluate(function() {
-                return document.querySelectorAll('.level_notch.selected')[1].getBoundingClientRect();
-              });
-              debug("Rect2 click : " + rect2.left + rect2.width / 2 + " / " + rect2.top + rect2.height / 2);
-              page.sendEvent('click', rect2.left + rect2.width / 2, rect2.top + rect2.height / 2);
-              setTimeout(function() {
-                var rect3 = page.evaluate(function(max) {
-                  return document.querySelector('#level_high' + max).getBoundingClientRect();
-                }, max);
-                debug("Rect3 click : " + rect3.left + rect3.width / 2 + " / " + rect3.top + rect3.height / 2)
-                page.sendEvent('click', rect3.left + rect3.width / 2, rect3.top + rect3.height / 2);
-                callback(null, true);
-              }, clickInterval)
-            }, clickInterval)
-          };
+    async.waterfall([
+      function(callback) {
+        debug("setMinMax : precheck");
+        var preresult = checkPortalLevel(min, max);
+        callback(null, preresult);
+      },
+      function(result, callback) {
+        debug("setMinMax : setmin");
+        if (result.isMinSame) {
+          debug("Min value is same");
+          callback(null, result);
+          return;
         }
-      }, function(err, results) {
-        console.log("Results : " + results.max + "/" + results.min);
-        if (results.max && results.min) {
-          page.evaluate(function() {
-            document.querySelector('#filters_container').style.display = 'none'
+        debug('Change Min value: ' + currentMin + '->' + min);
+        var rect = page.evaluate(function() {
+          return document.querySelectorAll('.level_notch.selected')[0].getBoundingClientRect();
+        });
+        debug("Rect click : " + rect.left + rect.width / 2 + " / " + rect.top + rect.height / 2);
+        page.sendEvent('click', rect.left + rect.width / 2, rect.top + rect.height / 2);
+        setTimeout(function() {
+          var rect1 = page.evaluate(function(min) {
+            return document.querySelector('#level_low' + min).getBoundingClientRect();
+          }, min);
+          debug("Rect1 click : " + rect1.left + rect1.width / 2 + " / " + rect1.top + rect1.height / 2);
+          page.sendEvent('click', rect1.left + rect1.width / 2, rect1.top + rect1.height / 2);
+          callback(null, result);
+        }, clickInterval);
+      },
+      function(result, callback) {
+        debug("setMinMax : setmax");
+        if (result.isMaxSame) {
+          debug("Max value is same");
+          callback(null, result);
+          return;
+        }
+        debug('Change Max value: ' + currentMax + '->' + max);
+        setTimeout(function() {
+          var rect2 = page.evaluate(function() {
+            return document.querySelectorAll('.level_notch.selected')[1].getBoundingClientRect();
           });
+          debug("Rect2 click : " + rect2.left + rect2.width / 2 + " / " + rect2.top + rect2.height / 2);
+          page.sendEvent('click', rect2.left + rect2.width / 2, rect2.top + rect2.height / 2);
+          setTimeout(function() {
+            var rect3 = page.evaluate(function(max) {
+              return document.querySelector('#level_high' + max).getBoundingClientRect();
+            }, max);
+            debug("Rect3 click : " + rect3.left + rect3.width / 2 + " / " + rect3.top + rect3.height / 2)
+            page.sendEvent('click', rect3.left + rect3.width / 2, rect3.top + rect3.height / 2);
+            callback(null, result);
+          }, clickInterval)
+        }, clickInterval)
+      },
+      function(result, callback) {
+        debug("setMinMax : postcheck");
+        if (result.isMinSame && result.isMaxSame) {
+          debug("Post check is OK");
+          callback(null, result);
+          return;
         }
-      })
-    }, clickInterval)
-  };
+        callback("Portal level is changed", result);
+      }
+    ], function(err, result) {
+      debug("Results : " + result.isMinSame + "/" + result.isMaxSame);
+      if (!err) {
+        page.evaluate(function() {
+          document.querySelector('#filters_container').style.display = 'none'
+        });
+        setCallback(null, true);
+      } else {
+        setCallback("Error!", false);
+      }
+    });
+  }
+
+  function checkPortalLevel(min, max) {
+    debug("Check avaiable value");
+    var minAvailable = page.evaluate(function() {
+      return document.querySelectorAll('.level_notch.selected')[0];
+    });
+    var maxAvailable = page.evaluate(function() {
+      return document.querySelectorAll('.level_notch.selected')[1];
+    });
+    var currentMinStr = minAvailable.id.replace(/level_(high|low)/g, "");
+    var currentMaxStr = maxAvailable.id.replace(/level_(high|low)/g, "");
+    currentMin = parseInt(currentMinStr, 10);
+    currentMax = parseInt(currentMaxStr, 10);
+    debug("Value : " + currentMin + "/" + currentMax);
+
+    var isMinSame = false;
+    var isMaxSame = false;
+    if (currentMin > min) {
+      debug('The minimal portal level is too low, using default. Consider setting it higher.');
+      isMinSame = true;
+    }
+    if (currentMin == min) {
+      debug('Min specified: ' + currentMin);
+      isMinSame = true;
+    }
+    if (currentMax == max) {
+      debug('Max specified: ' + currentMax);
+      isMaxSame = true;
+    }
+    var result = {
+      "isMinSame": isMinSame,
+      "isMaxSame": isMaxSame
+    };
+    return result;
+  }
 })();
